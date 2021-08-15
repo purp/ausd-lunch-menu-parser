a6455738b49756f84c319e768ead781827755683
Jim Meyer <jim@geekdaily.org>
2021-08-15 16:38:33 +0000

    Find and fetch links; failing on doc read

--- ausd-lunch-menu-parser.rb
+++ ausd-lunch-menu-parser.rb
@@ -5,6 +5,14 @@ require 'pdf-reader'
 
 menu_page = Nokogiri::HTML(URI.open('https://bit.ly/ausd-school-lunch-menus'))
 
-menu_links = menu_page.css('div.interactivemenuswrapper table tr td h3 div a')
+menu_links = menu_page.css('div.interactivemenuswrapper table tr td div a').select do |link|
+    link['href'].match(/downloadMenu.php/)
+end
 
-puts menu_links
+menu_links.select {|link| link['href'].match(/downloadMenu.php/)}.each do |link|
+    menu_url = 'https://www.schoolnutritionandfitness.com/' + link['href']
+    puts ">>> MENU URL: #{menu_url}"
+    menu_pdf = URI.open(menu_url)
+    menu_doc = PDF::Reader(menu_pdf)
+    puts menu_doc.info
+end
