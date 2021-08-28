4c921e88f37a6b5f20cf5acf9281a8d69e2a5d1f
Jim Meyer <jim@geekdaily.org>
2021-08-28 18:07:40 +0000

    checkpoint: fetching menus, now time to work out parsing

--- ausd-lunch-menu-parser.rb
+++ ausd-lunch-menu-parser.rb
@@ -10,13 +10,11 @@ menu_links = menu_page.css('div.interactivemenuswrapper table tr td div a').sele
     link['href'].match(/downloadMenu.php/)
 end
 
-menu_links.select {|link| link['href'].match(/downloadMenu.php/)}.each do |link|
-    menu_url = 'https://www.schoolnutritionandfitness.com' + link['href']
+menu_links.map {|elem| FinalRedirectUrl.final_redirect_url('https://www.schoolnutritionandfitness.com' + elem['href'])}.each do |menu_url|
     puts ">>> MENU URL: #{menu_url}"
-    final_menu_url = FinalRedirectUrl.final_redirect_url(menu_url)
-    puts ">>> FINAL MENU URL: #{final_menu_url}"
-    menu_pdf = URI.open(final_menu_url)
+    menu_pdf = URI.open(menu_url)
     menu_doc = PDF::Reader.new(menu_pdf)
     puts menu_doc.info
-    puts menu_doc.text
+    puts menu_doc.pages.size
+    puts menu_doc.pages.first.text
 end
